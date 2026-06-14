a65deb9e52dcf517595cc3da362b1a9f64aca2bd
Anne McGloughlin <annemcgloughlin@Annes-MacBook-Air.local>
2026-06-14 14:48:14 +0100

    Code for IceCreamParlor

--- topic4_hashmaps/IceCreamParlor.py
+++ topic4_hashmaps/IceCreamParlor.py
@@ -16,7 +16,14 @@ import sys
 #
 
 def icecreamParlor(m, arr):
-    # Write your code here
+    # [4,8,2,1,5,3]
+    price_map = {}
+    for i, price in enumerate(arr):
+        complement = m - price
+        if complement in price_map:
+            return [price_map[complement] + 1, i + 1]
+        price_map[price] = i
+    return []
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -24,11 +31,11 @@ if __name__ == '__main__':
     t = int(input().strip())
 
     for t_itr in range(t):
-        m = int(input().strip())
+        m = int(input().strip())# amount of money to spend 
 
-        n = int(input().strip())
+        n = int(input().strip())#number of flavors
 
-        arr = list(map(int, input().rstrip().split()))
+        arr = list(map(int, input().rstrip().split())) #cost of each flavor
 
         result = icecreamParlor(m, arr)
 
